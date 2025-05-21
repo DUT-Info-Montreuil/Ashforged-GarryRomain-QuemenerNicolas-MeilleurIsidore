2da2cbc5ba0cfd31fd210bbef7be230b00b1bf26
Romain <romain.garry@etud.univ-paris8.fr>
2025-05-21 14:30:32 +0200

    Ajout des collisions

--- src/main/java/universite_paris8/iut/rgarry/ashforged/Controller/PersonnageController.java
+++ src/main/java/universite_paris8/iut/rgarry/ashforged/Controller/PersonnageController.java
@@ -1,41 +1,40 @@
 package universite_paris8.iut.rgarry.ashforged.Controller;
+
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
+import javafx.scene.Node;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.TilePane;
 import universite_paris8.iut.rgarry.ashforged.model.character.Personnage;
 
 public class PersonnageController {
+
+    private static final String SKY_TILE_ID = "ciel";
+    private static final int BOUNDARY_OFFSET = 3;
 
     private final BooleanProperty spacePressed = new SimpleBooleanProperty();
     private final BooleanProperty qPressed = new SimpleBooleanProperty();
     private final BooleanProperty sPressed = new SimpleBooleanProperty();
     private final BooleanProperty dPressed = new SimpleBooleanProperty();
 
-    private static Personnage personnage;
-    private static Personnage paolo;
-    private static Personnage branda;
-    private static Personnage terry;
-    private static Personnage salome;
+    private final Personnage personnage;
+    private double velocityY = 0;
+    private static final double GRAVITY = 0.5;
+    private static final double JUMP_STRENGTH = -15;
 
-    private double velocityY;
-    private final double gravity = 0.5;
-    private final int GROUND_LEVEL = 150;
-    private final double jumpStrength = -15;
+    private final TilePane tilePane;
+    private final Pane panePerso;
 
-    public PersonnageController() {
-        // Creation of the main character
-        this.personnage = new Personnage("Hero", 15, new int[]{1, 1, 10, 1},600, 250);
+    private enum Direction { LEFT, RIGHT, TOP, BOTTOM }
 
-        // Creation of NPC
-        this.paolo = new Personnage("Paolo", 15, new int[]{1, 1, 10, 1},600, 250);
-        this.branda = new Personnage("Branda", 15, new int[]{1, 1, 10, 1},600, 250);
-        this.terry = new Personnage("Terry", 15, new int[]{1, 1, 10, 1},600, 250);
-        this.salome = new Personnage("Salome", 15, new int[]{1, 1, 10, 1},600, 250);
+    public PersonnageController(TilePane tilePane, Pane panePerso) {
+        this.personnage = new Personnage("Hero", 15, new int[]{1, 1, 10, 1}, 50, 300);
+        this.tilePane = tilePane;
+        this.panePerso = panePerso;
     }
 
-
-
-    public static Personnage getPersonnage() {
+    public Personnage getPersonnage() {
         return personnage;
     }
 
@@ -59,47 +58,124 @@ public class PersonnageController {
         });
     }
 
-    public void handleJump() {
-        if (personnage.getY() == GROUND_LEVEL) {
-            velocityY = jumpStrength;
+    public void applyGravity() {
+        velocityY += GRAVITY;
+        int steps = (int) Math.abs(velocityY);
+        int direction = velocityY > 0 ? 1 : -1;
+
+        for (int i = 0; i < steps; i++) {
+            if (direction > 0 && !checkCollision(Direction.BOTTOM)) {
+                personnage.setY(personnage.getY() + 1);
+            } else if (direction < 0 && !checkCollision(Direction.TOP)) {
+                personnage.setY(personnage.getY() - 1);
+            } else {
+                velocityY = 0;
+                break;
+            }
         }
     }
 
-    public void applyGravity() {
-        velocityY += gravity;
-        personnage.setY((int) (personnage.getY() + velocityY));
+    private boolean checkCollision(Direction direction) {
+        int deplacement = personnage.getVitesse();
+        var persoNodes = panePerso.getChildren().stream()
+                .filter(ImageView.class::isInstance)
+                .map(ImageView.class::cast)
+                .toList();
+        var tileNodes = tilePane.getChildren().stream()
+                .filter(ImageView.class::isInstance)
+                .map(ImageView.class::cast)
+                .filter(node -> !SKY_TILE_ID.equals(node.getId()))
+                .toList();
 
-        if (personnage.getY() > GROUND_LEVEL) {
-            personnage.setY(GROUND_LEVEL);
-            velocityY = 0;
+        for (Node persoNode : persoNodes) {
+            var bounds = persoNode.getBoundsInParent();
+            double nextMinX = bounds.getMinX();
+            double nextMaxX = bounds.getMaxX();
+            double nextMinY = bounds.getMinY();
+            double nextMaxY = bounds.getMaxY();
+
+            switch (direction) {
+                case LEFT -> nextMinX -= deplacement;
+                case RIGHT -> nextMaxX += deplacement;
+                case TOP -> nextMinY -= deplacement;
+                case BOTTOM -> nextMaxY += deplacement;
+            }
+
+            for (Node tileNode : tileNodes) {
+                var tileBounds = tileNode.getBoundsInParent();
+                boolean overlapX = nextMaxX > tileBounds.getMinX() && nextMinX < tileBounds.getMaxX();
+                boolean overlapY = nextMaxY > tileBounds.getMinY() && nextMinY < tileBounds.getMaxY();
+
+                if (overlapX && overlapY) {
+                    switch (direction) {
+                        case LEFT -> {
+                            personnage.setX((int) (tileBounds.getMaxX() + 1));
+                            System.out.println("Collision LEFT");
+                        }
+                        case RIGHT -> {
+                            personnage.setX((int) (tileBounds.getMinX() - bounds.getWidth()));
+                            System.out.println("Collision RIGHT");
+                        }
+                        case TOP -> {
+                            personnage.setY((int) tileBounds.getMaxY() + 1);
+                            System.out.println("Collision TOP");
+                        }
+                        case BOTTOM -> {
+                            personnage.setY((int) (tileBounds.getMinY() - bounds.getHeight()));
+                            System.out.println("Collision BOTTOM");
+                        }
+                    }
+                    return true;
+                }
+            }
         }
+        return false;
+    }
+
+    public boolean checkCollisionLeft()   { return checkCollision(Direction.LEFT); }
+    public boolean checkCollisionRight()  { return checkCollision(Direction.RIGHT); }
+    public boolean checkCollisionTop()    { return checkCollision(Direction.TOP); }
+    public boolean checkCollisionBottom() { return checkCollision(Direction.BOTTOM); }
+
+    public void handleJump() {
+        if (checkCollisionBottom() && velocityY == 0) {
+            velocityY = JUMP_STRENGTH;
+        }
+    }
+
+    private int getMaxX() {
+        return (int) tilePane.getWidth() + 1;
+    }
+    private int getMaxY() {
+        return (int) tilePane.getHeight();
     }
 
     public void handleLeft() {
-        personnage.deplacer('l');
+        if (!checkCollisionLeft()) {
+            personnage.deplacer('l', getMaxX(), getMaxY());
+        }
     }
-
     public void handleDown() {
-        personnage.deplacer('d');
+        if (!checkCollisionBottom()) {
+            personnage.deplacer('d', getMaxX(), getMaxY());
+        }
     }
-
     public void handleRight() {
-        personnage.deplacer('r');
+        double personnageWidth = panePerso.getChildren().stream()
+                .filter(ImageView.class::isInstance)
+                .mapToDouble(node -> node.getBoundsInParent().getWidth())
+                .max().orElse(0);
+
+        int maxX = getMaxX();
+        if (!checkCollisionRight() && personnage.getX() + personnage.getVitesse() + personnageWidth <= maxX - BOUNDARY_OFFSET) {
+            personnage.deplacer('r', maxX, getMaxY());
+        } else if (personnage.getX() + personnageWidth > maxX - BOUNDARY_OFFSET) {
+            personnage.setX((int) (maxX - personnageWidth));
+        }
     }
 
-    public boolean isSpacePressed() {
-        return spacePressed.get();
-    }
-
-    public boolean isQPressed() {
-        return qPressed.get();
-    }
-
-    public boolean isSPressed() {
-        return sPressed.get();
-    }
-
-    public boolean isDPressed() {
-        return dPressed.get();
-    }
+    public boolean isSpacePressed() { return spacePressed.get(); }
+    public boolean isQPressed()     { return qPressed.get(); }
+    public boolean isSPressed()     { return sPressed.get(); }
+    public boolean isDPressed()     { return dPressed.get(); }
 }
