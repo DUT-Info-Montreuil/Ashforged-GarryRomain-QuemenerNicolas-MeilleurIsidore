a467340e35a03f8c330c8b6e783276275416c5b9
Isidore <meilleurisidore13@gmail.com>
2025-06-19 15:03:21 +0200

    MAJ

--- src/main/java/universite_paris8/iut/rgarry/ashforged/model/Item/ItemInterface.java
+++ src/main/java/universite_paris8/iut/rgarry/ashforged/model/Item/ItemInterface.java
@@ -3,7 +3,7 @@ package universite_paris8.iut.rgarry.ashforged.model.Item;
 import javafx.scene.image.Image;
 
 public interface ItemInterface {
-    String getId();
+    int getId();
     String getName();
     int getWeight();
     String getImagePath();
